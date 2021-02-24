16c91ae8f25db53bcb93a3fdf7748d275a282350
eduardoMag <eduardobautista13@gmail.com>
2021-02-24 15:20:10 -0600

    adding scripts

---
+++ userAgent-test.py
@@ -0,0 +1,12 @@
+import mechanize
+
+def testUserAgent(url, userAgent):
+    browser = mechanize.Browser()
+    browser.addheaders = userAgent
+    page = browser.open(url)
+    source_code = page.read()
+    print(source_code)
+
+url = 'http://whatismyuseragent.dotdon.com/'
+userAgent = [('User-agent', 'Mozilla/5.0 (x11: u: '+\'Linux 2.4.2 2 1586: en-US: mIB) Gecko/2000131 Netscape6/6.01')]
+testUserAgent(usrl, userAgent)
